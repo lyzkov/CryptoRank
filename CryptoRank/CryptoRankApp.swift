0dfad82cd392a7df5d7adb49663006b3730a0014
Piotr Łyczba <lyzkov@gmail.com>
2021-09-09 14:37:29 +0200

    Create blank project

---
+++ CryptoRank/CryptoRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoRankApp.swift
+//  CryptoRank
+//
+//  Created by lyzkov on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
