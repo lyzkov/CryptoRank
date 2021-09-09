9daf239aa1305792e6de7da7351f846f17f71f63
Piotr Łyczba <lyzkov@gmail.com>
2021-09-09 22:19:18 +0200

    Auto refresh every 30 seconds

--- CryptoRank/Screens/CurrenciesList/CurrenciesListViewModel.swift
+++ CryptoRank/Screens/CurrenciesList/CurrenciesListViewModel.swift
@@ -17,8 +17,17 @@ final class CurrenciesListViewModel: ObservableObject {
 
     let tickersService = TickersService()
 
+    let refreshInterval: TimeInterval = 30
+
+    var category: CurrenciesCategory = .name
+
     init() {
-        tickersService.tickers()
+        Timer.publish(every: refreshInterval, on: .main, in: .default)
+            .autoconnect()
+            .prepend(Date())
+            .flatMap { [self] _ in
+                tickersService.tickers()
+            }
             .sink(receiveCompletion: { completion in
                 if case .failure(let error) = completion {
                     print(error)
@@ -27,12 +36,13 @@ final class CurrenciesListViewModel: ObservableObject {
                 self.items = tickers.data.map({ datum in
                     CurrenciesListItem(from: datum)
                 })
-                sort(by: .name)
+                sort(by: category)
             })
             .store(in: &cancellable)
     }
 
     func sort(by category: CurrenciesCategory) {
+        self.category = category
         items.sort { lhs, rhs in
             switch category {
             case .name:
